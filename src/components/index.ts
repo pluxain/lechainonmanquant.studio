80ef9d6bdabe4513215472adf7bee9cc098349bb
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-15 17:10:30 +0100

    feat(content): add basic footer
    Ref: #9

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
+import Footer from "./Footer.astro";
 import Header from "./Header.astro";
 import LanguageSwitcher from "./LanguageSwitcher.astro";
 import LanguageTag from "./LanguageTag.astro";
 import Link from "./Link.astro";
 
-export { Header, LanguageSwitcher, LanguageTag, Link };
+export { Footer, Header, LanguageSwitcher, LanguageTag, Link };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
 export default {
   content: ["./src/**/*.{astro,html,ts,tsx}"],
   theme: {
+    colors: {
+      primary: {
+        accent: colors.blue[300],
+        DEFAULT: colors.blue[500],
+        solid: colors.blue[50],
+      },
+    },
     extend: {},
   },
   plugins: [],
