8d9ddefa4efa7f13220f66fe660ed07225cbe5f1
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-15 14:54:59 +0100

    feat(content): add LanguageSwitcher component
    Ref: #3

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import LanguageSwitcher from "./LanguageSwitcher.astro";
+import LanguageTag from "./LanguageTag.astro";
+import Link from "./Link.astro";
+
+export { LanguageSwitcher, LanguageTag, Link };
