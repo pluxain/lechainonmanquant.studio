0f0c9dfeff866bcea5c6bd7666f9a00dfb1b3a7b
Pascal Luxain <pascal.luxain@gmail.com>
2024-12-12 08:40:13 +0100

    feat(design): define bigger font sizes and improve content parsing
    Ref: #13

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 import colors from "tailwindcss/colors";
 export default {
-  content: ["./src/**/*.{astro,html,ts,tsx}"],
+  content: [
+    "./src/components/**/*.{astro,html,tsx}",
+    "./src/layouts/**/*.{astro,html,tsx}",
+    "./src/pages/**/*.{astro,html,tsx}",
+    "./src/styles/**/*.css",
+  ],
   theme: {
     colors: {
       primary: {
@@ -9,6 +14,16 @@ export default {
         DEFAULT: colors.blue[500],
         solid: colors.blue[50],
       },
+    },
+    fontSize: {
+      xs: ["0.75rem", { lineHeight: "1rem" }],
+      sm: ["0.875rem", { lineHeight: "1.25rem" }],
+      base: ["1.25rem", { lineHeight: "1.75rem" }],
+      lg: ["1.5rem", { lineHeight: "2rem" }],
+      xl: ["1.875rem", { lineHeight: "2.25rem" }],
+      "2xl": ["2.25rem", { lineHeight: "2.5rem" }],
+      "3xl": ["3rem", { lineHeight: "1" }],
+      "4xl": ["3.75rem", { lineHeight: "1" }],
     },
     extend: {
       fontFamily: {
