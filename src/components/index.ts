de1591a06f8523009295cf440fca062efb360080
Pascal Luxain <pascal.luxain@gmail.com>
2024-12-12 14:46:53 +0100

    ref(design): rename logo Component and use the new styling approach
    - add icon Component
    
    Ref: #15

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,15 @@ import Header from "./Header.astro";
 import LanguageSwitcher from "./LanguageSwitcher.astro";
 import LanguageTag from "./LanguageTag.astro";
 import Link from "./Link.astro";
-import Logo from "./Logo.astro";
+import LcnmqtIcon from "./LcnmqtIcon.astro";
+import LcnmqtLogo from "./LcnmqtLogo.astro";
 
-export { Footer, Header, LanguageSwitcher, LanguageTag, Link, Logo };
+export {
+  Footer,
+  Header,
+  LanguageSwitcher,
+  LanguageTag,
+  Link,
+  LcnmqtIcon,
+  LcnmqtLogo,
+};
